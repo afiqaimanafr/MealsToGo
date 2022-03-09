fc880bde515e2b1bb1a91fb5a270f3e1a906ee64
afiqaimanafr <aaafr98@gmail.com>
2022-03-09 20:02:45 +0800

    latest

--- App.js
+++ App.js
@@ -1,14 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View>
-      
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>First React Native App</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  
-});
+const styles = StyleSheet.create({});
