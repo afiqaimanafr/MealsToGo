f98aa7bb233d8bb90684faed17238778596ac613
afiqaimanafr <aaafr98@gmail.com>
2022-03-10 13:41:41 +0800

    latest

--- src/features/restaurants/components/restaurant-info-card-component.js
+++ src/features/restaurants/components/restaurant-info-card-component.js
@@ -1,10 +1,19 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text } from "react-native";
 import { Card } from "react-native-paper";
 import styled from "styled-components/native";
 
 const Title = styled.Text`
   padding: 16px;
+`;
+
+const RestaurantCard = styled(Card)`
+  background-color: white;
+`;
+
+const RestaurantCardCover = styled(Card.Cover)`
+  padding: 20px;
+  background-color: white;
 `;
 
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
@@ -20,14 +29,12 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
     isClosedTemporarily,
   } = restaurant;
   return (
-    <Card elevation={5} style={styles.card}>
-      <Card.Cover key={name} style={styles.cover} source={{ uri: photos[0] }} />
+    <RestaurantCard elevation={5}>
+      <RestaurantCardCover
+        key={name}
+        source={{ uri: photos[0] }}
+      />
       <Title>{name}</Title>
-    </Card>
+    </RestaurantCard>
   );
 };
-
-const styles = StyleSheet.create({
-  card: { backgroundColor: "white" },
-  cover: { padding: 20, backgroundColor: "white" },
-});
