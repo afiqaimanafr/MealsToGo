c2f1081f7e9e786dd35f3ad66bf252255a533f79
afiqaimanafr <aaafr98@gmail.com>
2022-03-10 13:55:00 +0800

    latest

--- src/features/restaurants/screens/restaurants-screen.js
+++ src/features/restaurants/screens/restaurants-screen.js
@@ -13,7 +13,7 @@ import styled from "styled-components/native";
 
 const SafeArea = styled(SafeAreaView)`
   flex: 1;
-  margin-top: ${StatusBar.currentHeight}px;
+  ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`};
 `;
 
 const SearchContainer = styled.View`
