7726ac8bf847b3c8b8c19a7d393eeb5e972243b1
afiqaimanafr <aaafr98@gmail.com>
2022-03-15 11:52:52 +0800

    latest update

---
+++ src/components/spacer/spacer-component.js
@@ -0,0 +1,13 @@
+import react from "react";
+import { View } from "react-native";
+import styled from "styled-components/native";
+
+const TopSmall = styled.View`
+  margintop: 4px;
+`;
+
+export const Spacer = ({ variant }) => {
+  switch (variant) {
+    default:
+  }
+};
